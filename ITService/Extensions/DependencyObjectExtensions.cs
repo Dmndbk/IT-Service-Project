33a0e34a1e24a20f0941b6007ed418baf185d01e
leonid <leonidpoznakhirev@gmail.com>
2024-05-01 18:45:36 +0300

    Методы-расширения для поиска элемента среди родительских в визуальном или логическом дереве

---
+++ ITService/Extensions/DependencyObjectExtensions.cs
@@ -0,0 +1,60 @@
+﻿using System.Windows;
+using System.Windows.Media;
+
+namespace System.Windows
+{
+    static class DependencyObjectExtensions
+    {
+        public static DependencyObject FindVisualRoot(this DependencyObject element)
+        {
+            do
+            {
+                var parent = VisualTreeHelper.GetParent(element);
+                if (parent is null) return element;
+                element = parent;
+            }
+            while (true);
+        }
+
+        public static DependencyObject FindLogicalRoot(this DependencyObject obj)
+        {
+            do
+            {
+                var parent = LogicalTreeHelper.GetParent(obj);
+                if (parent is null) return obj;
+                obj = parent;
+            }
+            while (true);
+        }
+
+        public static T? FindVisualParent<T>(this DependencyObject? obj) where T : DependencyObject
+        {
+            if (obj is null) return null;
+            var target = obj;
+
+            do
+            {
+                target = VisualTreeHelper.GetParent(target);
+            } 
+            while (target != null && target is not T);
+            
+            return target as T;
+        }
+
+        public static T? FindLogicalParent<T>(this DependencyObject? obj) where T : DependencyObject
+        {
+            if (obj is null) return null;
+            var target = obj;
+
+            do
+            {
+                target = LogicalTreeHelper.GetParent(target);   
+            } 
+            while (target != null && target is not T);
+            
+            return target as T;
+        }
+    }
+
+
+}
