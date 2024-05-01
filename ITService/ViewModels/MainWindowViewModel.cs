98e4a9ed02e6e21ecd37e385bae0c01c506de6b2
leonid <leonidpoznakhirev@gmail.com>
2024-05-01 16:26:47 +0300

    Добавьте файлы проекта.

---
+++ ITService/ViewModels/MainWindowViewModel.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ITService.ViewModels
+{
+    class MainWindowViewModel : ViewModelBase
+    {
+        private string _Title = "IT-Service";
+        public string Title
+        {
+            get => _Title;
+            //set
+            //{
+            //    //if(Equals(_Title, value)) return;
+            //    //_Title = value;
+            //    //OnPropertyChanged();
+
+            //    Set(ref _Title, value)
+            //}
+            set => Set(ref _Title, value);
+        }
+    }
+}
